c5630365b164cfec408fe2071330c9fe9492c052
hershel <daniel.h.silber.baker@gmail.com>
2017-07-05 17:23:22 -0400

    uploading first draft of p5 velocity blast template

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var canvas;
+
+// maybe add function resize? See how this will work with responsive css
+
+function setup() {
+canvas = createCanvas(200, 200);
+canvas.position(30, 50);
+background('#239B6F')
+}
+
+
+
+function draw(){
+
+
+}
